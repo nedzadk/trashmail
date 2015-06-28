d5dfdf687669bdf51c2064f91badd8eb318762b6
Nedzad Kulelija <nedzadk@gmail.com>
2015-06-28 18:28:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trashmail::Application.config.secret_token = '2d42e8d32d69e18066632ab46b7315814ef5cb5f9546108724253633c067ac939f4578d43f32c644e7076a50cb249d513da980dd2b06d548c9e2becc714d38ab'
